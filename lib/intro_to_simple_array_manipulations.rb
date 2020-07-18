8a2ada962a4c81fd459773f8208b8928bfda749a
Sayana <nisayana23@yandex.ru>
2020-07-18 21:35:22 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,17 @@
+def using_concat(arg1, arg2)
+  arg1.concat(arg2)
+end
+
+def using_insert(array, elem)
+  array.insert(4, elem)
+end
+
+def using_uniq(array)
+  new_array = array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+# def using_delete(array, string)
